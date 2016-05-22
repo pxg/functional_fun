bf5de99316da6d1e02185483c39a8bd9ebba1bce
Pete Graham <git@petegraham.co.uk>
2016-05-22 16:28:15 +0100

    Asking if person was born after May 22nd
    Getting correct age of 32 for me now.

--- how_old_am_i.hs
+++ how_old_am_i.hs
@@ -1,12 +1,20 @@
 {-#LANGUAGE ScopedTypeVariables#-}
--- Gives 33 for input of 1983 which is incorrect
-how_old n = 2016 - n
+how_old n True = 2016 - n - 1
+how_old n False = 2016 - n
+
+after_today ['y'] = True
+after_today n = False
 
 
 main = do
     putStrLn "What year were you born in?"
-    user_input <- getLine
+    year_input <- getLine
     -- TODO: Add validation here.
     -- Casting technique from http://stackoverflow.com/questions/11481306/haskell-how-to-parse-an-io-input-string-into-a-float-or-int-or-whatever
-    let year_of_birth = read user_input :: Int
-    print (how_old year_of_birth)
+    let year_of_birth = read year_input :: Int
+
+    putStrLn "Where you born after May 22nd? Answer 'y' for yes"
+    date_input <- getLine
+    let date = after_today(date_input)
+
+    print (how_old year_of_birth date)
