20c02496a31991b3221b7c7f34f37c0ca5d2da7e
Pete Graham <git@petegraham.co.uk>
2016-05-22 16:14:16 +0100

    How old using do syntax

--- how_old_am_i.hs
+++ how_old_am_i.hs
@@ -1,3 +1,12 @@
-how_old 1983 = 32
+-- Gives 33 for input of 1983 which is incorrect
+how_old n = 2016 - n
 
-main = print (how_old 1983)
+
+main = do
+    print (how_old 1983)
+    --putStrLn "What year were you born in?"
+    --year_of_birth <- getLine
+    -- Need validation on the string here
+    -- print (how_old year_of_birth)
+    --age =
+    --putStrLn ("You were born in " ++ year_of_birth)
