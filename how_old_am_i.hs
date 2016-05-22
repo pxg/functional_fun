697b03ded10b68d590243afbdb69828ce5070034
Pete Graham <git@petegraham.co.uk>
2016-05-22 16:22:21 +0100

    Getting year of birth as user input

--- how_old_am_i.hs
+++ how_old_am_i.hs
@@ -1,12 +1,12 @@
+{-#LANGUAGE ScopedTypeVariables#-}
 -- Gives 33 for input of 1983 which is incorrect
 how_old n = 2016 - n
 
 
 main = do
-    print (how_old 1983)
-    --putStrLn "What year were you born in?"
-    --year_of_birth <- getLine
-    -- Need validation on the string here
-    -- print (how_old year_of_birth)
-    --age =
-    --putStrLn ("You were born in " ++ year_of_birth)
+    putStrLn "What year were you born in?"
+    user_input <- getLine
+    -- TODO: Add validation here.
+    -- Casting technique from http://stackoverflow.com/questions/11481306/haskell-how-to-parse-an-io-input-string-into-a-float-or-int-or-whatever
+    let year_of_birth = read user_input :: Int
+    print (how_old year_of_birth)
