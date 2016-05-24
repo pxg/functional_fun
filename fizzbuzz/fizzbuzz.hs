7494b96bd2a27041218c751029b0140394e91c96
Pete Graham <git@petegraham.co.uk>
2016-05-24 09:07:29 +0100

    FizzBuzz 1 to 100

--- fizzbuzz/fizzbuzz.hs
+++ fizzbuzz/fizzbuzz.hs
@@ -7,13 +7,8 @@ fizzbuzz x = if (fizz x && buzz x)
         else if (buzz x)
             then "Buzz"
             else show x
-fizzbuzz_display x = "fizzbuzz " ++ show x ++ " " ++ fizzbuzz x
+fizzbuzz_display x = "fizzbuzz " ++ show x ++ " " ++ fizzbuzz x ++ "\n"
 
 main = do
-    print(fizzbuzz_display 1)
-    print(fizzbuzz_display 2)
-    print(fizzbuzz_display 3)
-    print(fizzbuzz_display 4)
-    print(fizzbuzz_display 5)
-    print(fizzbuzz_display 6)
-    print(fizzbuzz_display 15)
+    let xs = [fizzbuzz_display x | x <- [1..100]]
+    print xs
