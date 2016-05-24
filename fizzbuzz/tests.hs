12efddebbe66b211b6f688d2743f12b064434571
Pete Graham <git@petegraham.co.uk>
2016-05-24 09:46:43 +0100

    Fizz test working but import isn't

--- fizzbuzz/tests.hs
+++ fizzbuzz/tests.hs
@@ -1,10 +1,16 @@
 module Main where
 
 import Test.HUnit
+--TODO: work out how to import from Fizzbuzz
 
-derp = test [ "a silly test" ~: 'a' ~=? 'a' ]
--- test2 = test [ "pete's silly test" True True]
-tests = TestList [ derp ]
+fizz x = x `mod` 3 == 0
+
+--
+derp_1 = test [ "a silly test" ~: 'a' ~=? 'a' ]
+derp_2= test [ "fizz 1 test" ~: fizz 1 ~=? False ]
+derp_3= test [ "fizz 2 test" ~: fizz 2 ~=? False ]
+derp_4= test [ "fizz 3 test" ~: fizz 3 ~=? True ]
+tests = TestList [derp_1, derp_2, derp_3, derp_4]
 
 main::IO()
 main = (runTestTT tests) >>= (\x -> putStrLn $ show x)
