1c51c64058704a1017d124c354adac55c8261db3
Pete Graham <git@petegraham.co.uk>
2016-05-24 08:54:41 +0100

    Buzz logic added

--- fizzbuzz/fizzbuzz.hs
+++ fizzbuzz/fizzbuzz.hs
@@ -1,5 +1,15 @@
-fizzbuzz x = if (x `mod` 3 == 0) then "Fizz" else show x
+fizz x = x `mod` 3 == 0
+buzz x = x `mod` 5 == 0
+-- TODO: multiline for the if else
+fizzbuzz x = if (fizz x) then "Fizz" else if (buzz x) then "Buzz" else show x
 
 
 main = do
+    -- TODO: say what we're printing here (use function)
     print(fizzbuzz 1)
+    print(fizzbuzz 2)
+    print(fizzbuzz 3)
+    print(fizzbuzz 4)
+    print(fizzbuzz 5)
+    print(fizzbuzz 6)
+    print(fizzbuzz 15)
