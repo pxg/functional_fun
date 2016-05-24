5a39ade2b9b63b72bf06b5977c1cbdb4abadc964
Pete Graham <git@petegraham.co.uk>
2016-05-24 09:48:51 +0100

    Tests in list instead of variables

--- fizzbuzz/tests.hs
+++ fizzbuzz/tests.hs
@@ -5,12 +5,12 @@ import Test.HUnit
 
 fizz x = x `mod` 3 == 0
 
---
-derp_1 = test [ "a silly test" ~: 'a' ~=? 'a' ]
-derp_2= test [ "fizz 1 test" ~: fizz 1 ~=? False ]
-derp_3= test [ "fizz 2 test" ~: fizz 2 ~=? False ]
-derp_4= test [ "fizz 3 test" ~: fizz 3 ~=? True ]
-tests = TestList [derp_1, derp_2, derp_3, derp_4]
+-- TODO:
+derp_1 = test [ "a silly test" ~: 'a' ~=? 'a',
+    "fizz 1 test" ~: fizz 1 ~=? False,
+    "fizz 2 test" ~: fizz 2 ~=? False,
+    "fizz 3 test" ~: fizz 3 ~=? True ]
+tests = TestList [derp_1]
 
 main::IO()
 main = (runTestTT tests) >>= (\x -> putStrLn $ show x)
