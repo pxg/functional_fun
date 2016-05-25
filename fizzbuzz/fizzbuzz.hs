ff4dbf82f6778e02b4a44364a52508cf2365b07c
Pete Graham <git@petegraham.co.uk>
2016-05-25 09:23:08 +0100

    Added function signatures for Fizzbuzz

--- fizzbuzz/fizzbuzz.hs
+++ fizzbuzz/fizzbuzz.hs
@@ -1,7 +1,12 @@
 module Fizzbuzz where
 
+fizz :: Integral a => a -> Bool
 fizz x = x `mod` 3 == 0
+
+buzz :: Integral a => a -> Bool
 buzz x = x `mod` 5 == 0
+
+fizzbuzz :: (Integral a, Show a) => a -> [Char]
 fizzbuzz x = if (fizz x && buzz x)
     then "FizzBuzz"
     else if (fizz x)
@@ -9,4 +14,6 @@ fizzbuzz x = if (fizz x && buzz x)
         else if (buzz x)
             then "Buzz"
             else show x
+
+fizzbuzz_display :: (Integral a, Show a) => a -> [Char]
 fizzbuzz_display x = "fizzbuzz " ++ show x ++ " " ++ fizzbuzz x
