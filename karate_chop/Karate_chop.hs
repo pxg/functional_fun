a157bdd5e80efe5e6fb10f5dddcae5e8d374c61d
Pete Graham <git@petegraham.co.uk>
2016-05-31 18:09:15 +0100

    Clearer logic

--- karate_chop/Karate_chop.hs
+++ karate_chop/Karate_chop.hs
@@ -7,13 +7,11 @@ chop n [x] = if x == n then 0 else -1
 chop n my_list
     | n == middle_item = middle_index
     | n < middle_item =
-        if (chop n left_list) == -1 -- TODO: variable here?
-        then -1
-        else middle_index - (length(left_list) - chop n left_list)
+        if (chop n left_list) == -1 then -1
+        else chop n left_list
     | n > middle_item =
-        if (chop n right_list) == -1
-        then -1
-        else middle_index + chop n right_list + 1
+        if (chop n right_list) == -1 then -1
+        else middle_index + 1 + chop n right_list
     where middle_index = length(my_list) `div` 2
           middle_item = my_list !! middle_index
           left_list = take middle_index my_list
